72adefe6b4424c7d94b91f4060491f3725c82413
李敏 <sclimin@qq.com>
2018-08-08 13:11:56 +0800

    no message

--- library/src/java/com/sclimin/mykit/widget/MaskImageView.java
+++ library/src/java/com/sclimin/mykit/widget/MaskImageView.java
@@ -2,32 +2,25 @@ package com.sclimin.mykit.widget;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
 
 /**
  * 作者：limin
  * <p>
- * 创建时间：2018/03/28
+ * 创建时间：2018/08/8
  */
-
-public class MaskButton extends AppCompatButton {
+public class MaskImageView extends AppCompatImageView {
 
     private final MaskLayerHelper mMaskHelper;
 
-    public MaskButton(Context context, AttributeSet attrs) {
+    public MaskImageView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public MaskButton(Context context, AttributeSet attrs, int defStyleAttr) {
+    public MaskImageView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         mMaskHelper = new MaskLayerHelper(this, attrs, defStyleAttr);
-        mMaskHelper.drawableState(getDrawableState());
-    }
-
-    @Override
-    protected void drawableStateChanged() {
-        super.drawableStateChanged();
         mMaskHelper.drawableState(getDrawableState());
     }
 
@@ -38,7 +31,14 @@ public class MaskButton extends AppCompatButton {
     }
 
     @Override
+    protected void drawableStateChanged() {
+        super.drawableStateChanged();
+        mMaskHelper.drawableState(getDrawableState());
+    }
+
+    @Override
     public void draw(Canvas canvas) {
+        super.draw(canvas);
         mMaskHelper.draw(canvas, super::draw);
     }
 }
