dfd2b51a85dfa6e27527df69bfb955575b613e2a
李敏 <sclimin@qq.com>
2018-08-08 16:10:48 +0800

    no message

---
+++ library/src/java/com/sclimin/mykit/widget/MaskTextView.java
@@ -0,0 +1,43 @@
+package com.sclimin.mykit.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+
+/**
+ * 作者：limin
+ * <p>
+ * 创建时间：2018/08/8
+ */
+public class MaskTextView extends AppCompatTextView {
+
+    private final MaskLayerHelper mMaskHelper;
+
+    public MaskTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MaskTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mMaskHelper = new MaskLayerHelper(this, attrs, defStyleAttr);
+        mMaskHelper.drawableState(getDrawableState());
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int ow, int oh) {
+        super.onSizeChanged(w, h, ow, oh);
+        mMaskHelper.sizeChange(w, h);
+    }
+
+    @Override
+    protected void drawableStateChanged() {
+        super.drawableStateChanged();
+        mMaskHelper.drawableState(getDrawableState());
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        mMaskHelper.draw(canvas, super::draw);
+    }
+}
